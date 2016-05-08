a5c3652232743898adad79f347dd5b354d1981f4
Roman Chvanikov <chvanikoff@gmail.com>
2016-05-08 06:34:07 +0300

    Hello DevTools

--- web/static/js/store/index.js
+++ web/static/js/store/index.js
@@ -1,15 +1,22 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunkMiddleware from "redux-thunk";
 
 import reducers from "reducers";
 import WSActions from "actions/ws";
 
 
+const devToolsExt = typeof window === "object" && typeof window.devToolsExtension !== "undefined"
+  ? window.devToolsExtension()
+  : f => f;
+
 export default function configureStore(initialState) {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(thunkMiddleware),
+    compose(
+      applyMiddleware(thunkMiddleware),
+      devToolsExt
+    )
   );
   if (typeof window !== "undefined") {
     store.dispatch(WSActions.socket_connect());
